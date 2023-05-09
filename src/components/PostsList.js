b94e13b304eee5f70612eebc58a25da71b4dff6f
tian-shu-ASUS <tianshumild@gmail.com>
2023-05-09 00:23:38 -0400

    complete routing

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,37 +1,24 @@
-import React, { useState } from 'react';
-
-import NewPost from './NewPost';
 import Post from './Post';
-import Modal from './Modal';
 import classes from './PostsList.module.css';
+import { useLoaderData } from 'react-router-dom';
 
 function PostsList(props) {
-  const [posts, setPosts] = useState([]);
-
-  function addPostHandler(postData) {
-    setPosts((prevPosts)=>{
-      return [postData, ...prevPosts];
-    });
-  };
-
+  const posts = useLoaderData();
+  
   return (
     <>
-      {props.isPosting && 
-        (<Modal onClose={props.onStopPosting}>
-          <NewPost onCancel = {props.onStopPosting} onAddPost={addPostHandler}/>
-        </Modal>)
+      {
+        posts.length > 0 ? (
+            <ul className={classes.posts}>
+              {posts.map((post) => <Post key={post.id} id={post.id} author = {post.author} body = {post.body}/>)}
+            </ul>
+          ):(
+            <div style={{ textAlign: 'center', color: 'white' }}>
+              <h2>There are no posts yet.</h2>
+              <p>Start adding some!</p>
+            </div>
+        )
       }
-      
-      {posts.length > 0 ? (
-          <ul className={classes.posts}>
-            {posts.map((post) => <Post key={post.body} author = {post.author} body = {post.body}/>)}
-          </ul>
-        ):(
-          <div style={{ textAlign: 'center', color: 'white' }}>
-            <h2>There are no posts yet.</h2>
-            <p>Start adding some!</p>
-          </div>
-      )}
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import './index.css';
+import NewPost, { action as newPostAction} from './routes/NewPost';
+import RootLayout from './routes/RootLayout';
+import Posts, {loader as postsLoader} from './routes/Posts';
+import PostDetails, {loader as postDetailsLoader} from './routes/PostDetails';
+
+const router = createBrowserRouter([
+    { path: '/', element: <RootLayout/>, children: [
+        { path: '/', element: <Posts />, loader: postsLoader, children: [
+            { path: '/create-post', element: <NewPost />, action: newPostAction},
+            { path: '/:postId', element: <PostDetails />, loader: postDetailsLoader}
+        ] }
+    ]},
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
